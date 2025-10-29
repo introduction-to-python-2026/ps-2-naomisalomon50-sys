1a1bfab1e36272273932ade14da60383c6a9aa55
naomisalomon50-sys <naomi.salomon50@gmail.com>
2025-10-29 20:13:42 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,11 +1,11 @@
-   def find_max_number(num1, num2, num3):
+  def find_max_number(num1, num2, num3):
     if num1 > num2 and num1 > num3:
      return num1
-    if num2 > num1 and num2 > num3:
+    elif num2 > num1 and num2 > num3:
      return num2
-      return num3
+    else: return num3
 
-    def find_mean(num1, num2, num3):
+ def find_mean(num1, num2, num3):
     return (num1 + num2 + num3)/3
     
 def find_mean_std(num1, num2, num3):
